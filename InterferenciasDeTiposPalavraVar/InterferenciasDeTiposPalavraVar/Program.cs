68d71568c7da5a3dbb581e922e2746ec61426262
juvenciolinhares <jlinhares@cpqi.com>
2022-07-02 16:56:50 -0300

    variavel var

---
+++ InterferenciasDeTiposPalavraVar/InterferenciasDeTiposPalavraVar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InterferenciasDeTiposPalavraVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var x = 10;//o compilador entende que é um inteiro
+            var y = 20.0;
+            var z = "Maria";
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
+/*
+ * PALAVRA VAR:
+ * entra no lugar do tipo:
+ * 
+ */

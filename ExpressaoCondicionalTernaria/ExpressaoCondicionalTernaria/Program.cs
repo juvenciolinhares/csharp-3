6966132d9989097882f99b772985b4034dd1fd03
juvenciolinhares <jlinhares@cpqi.com>
2022-07-02 17:19:24 -0300

    Expressao Condicional Ternaria

---
+++ ExpressaoCondicionalTernaria/ExpressaoCondicionalTernaria/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExpressaoCondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*sintaxe:
+             * (condicao)? valor se verdadeiro : valor se falso;
+             */
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
+/*
+ * ESTRUTURA OPCIONAL AO IF-ELSE
+ * quando se deseja decidir um valor com base em uma condição.
+ * sintaxe:
+ * (condicao)? valor se verdadeiro : valor se falso;
+ */

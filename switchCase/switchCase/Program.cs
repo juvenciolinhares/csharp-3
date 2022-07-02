d0cbf785d782d6b3b1ddd1b99246b9c0a7493d59
juvenciolinhares <jlinhares@cpqi.com>
2022-07-02 17:09:06 -0300

    switch case

---
+++ switchCase/switchCase/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace switchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x)
+            {
+                case 1:
+                    day = "sunday";
+                    break;
+
+                case 2:
+                    day = "monday";
+                    break;
+
+                case 3:
+                    day = "tuesday";
+                    break;
+                case 4:
+                    day = "wednesday";
+                        break;
+                case 5:
+                    day = "thurdsday";
+                    break;
+                case 6:
+                    day = "saturday";
+                    break;
+                case 7:
+                    day = "saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+
+
+            }
+            Console.WriteLine("Day: " +day);
+        }
+    }
+}
+/*
+ * ESTRUTURA OPCIONAL AO IF-ELSE
+ * quando envolve teste do valor de uma variavel
+ */
